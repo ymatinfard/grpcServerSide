1ea86ad5b72e55722f3cac74ebcb04261b909b4b
Yousef <matinfard.y@gmail.com>
2025-03-26 23:26:23 +0100

    Fix Grpc bean issue
    - Grpc service is not registered correctly using only @GrpcService. Then bean added

--- src/main/kotlin/com/matin/happychat/Configuration.kt
+++ src/main/kotlin/com/matin/happychat/Configuration.kt
@@ -1,13 +1,15 @@
 package com.matin.happychat
 
 import ChatGrpcService
+import com.matin.happychat.service.ChatService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class GrpcConfig {
+
     @Bean
-    fun grpcService(): ChatGrpcService {
-        return ChatGrpcService()
+    fun grpcService(chatService: ChatService): ChatGrpcService {
+        return ChatGrpcService(chatService)
     }
 }
--- src/main/kotlin/com/matin/happychat/service/ChatGrpcService.kt
+++ src/main/kotlin/com/matin/happychat/service/ChatGrpcService.kt
@@ -1,19 +1,21 @@
 import com.matin.happychat.ChatMessage
 import com.matin.happychat.ChatServiceGrpcKt
+import com.matin.happychat.service.ChatService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import net.devh.boot.grpc.server.service.GrpcService
 
 @GrpcService
-class ChatGrpcService : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
+class ChatGrpcService(private val chatService: ChatService) : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
 
     override fun chatStream(requests: Flow<ChatMessage>): Flow<ChatMessage> {
         return flow {
             requests.collect { request ->
-                println("From client: ${request.message}")  // Log the message received
+                println("From client: ${request.message}")
+                chatService.saveMessage(request)
                 emit(
                     ChatMessage.newBuilder()
-                        .setMessage("Echo: ${request.message}")  // Echo message back to the client
+                        .setMessage("Echo: ${request.message}")
                         .build()
                 )
             }
